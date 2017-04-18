28a702a4bbd7fc0f22dffd10ed133cc93f359464
Raghav Bhatnagar <raghavbhatnagar96@gmail.com>
2017-04-18 16:59:50 +0530

    Uploading files

---
+++ Objects/lis302dl.d
@@ -0,0 +1,4 @@
+.\objects\lis302dl.o: Source\lis302dl.c
+.\objects\lis302dl.o: .\Inc\lis302dl.h
+.\objects\lis302dl.o: .\Inc\accelero.h
+.\objects\lis302dl.o: E:\Kiel_5\ARM\ARMCC\Bin\..\include\stdint.h
---
+++ Objects/lis3dsh.d
@@ -0,0 +1,4 @@
+.\objects\lis3dsh.o: Source\lis3dsh.c
+.\objects\lis3dsh.o: .\Inc\lis3dsh.h
+.\objects\lis3dsh.o: .\Inc\accelero.h
+.\objects\lis3dsh.o: E:\Kiel_5\ARM\ARMCC\Bin\..\include\stdint.h
